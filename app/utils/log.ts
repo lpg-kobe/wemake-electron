5c79f059a39ff71eb0db993684c9a887caa547fe
lpg-kobe <34902006+lpg-kobe@users.noreply.github.com>
2021-02-23 18:27:17 +0800

    Update log.ts

--- app/utils/log.ts
+++ app/utils/log.ts
@@ -1,73 +1,74 @@
-
 /**
- * @desc main logger during app during running,base on winston
+ * @desc main logger during app running,base on winston
  * @author pika
  */
 
 const winston = require('winston')
 const util = require('util')
 const chalk = require('chalk')
+const { NODE_ENV, RESOURCES_PATH } = require('../constants')
 const levels = winston.config.npm.levels
+const logFolder = NODE_ENV === 'production' ? `${RESOURCES_PATH}/log` : 'release/log'
 
 require('winston-daily-rotate-file')
 
 const {
-  format: { splat, colorize, printf, combine, label, timestamp },
-  Transport,
-  transports,
-  createLogger
+    format: { splat, colorize, printf, combine, label, timestamp },
+    Transport,
+    transports,
+    createLogger
 } = winston
 
-const wemakeLog: any = createLogger({
-  level: 'info',
-  levels,
-  exitOnError: false, // winston will not exit if handled exceptions
-  format: combine(
-    colorize(),
-    label({ label: 'wemake' }),
-    timestamp(),
-    splat(),
-    printf(({ message, level, label, timestamp }: any) => `${label}:${timestamp} ${level} ==== ${message}`)
-  ),
-  transports: [
-    // log in console of chrome
-    new transports.Console({
-      level: 'error',
-    }),
+const ofweekLog: any = createLogger({
+    level: 'info',
+    levels,
+    exitOnError: false, // winston will not exit if handled exceptions
+    format: combine(
+        colorize(),
+        label({ label: 'OFweek' }),
+        timestamp(),
+        splat(),
+        printf(({ message, level, label, timestamp }: any) => `${label}:${new Date(timestamp)} ${level} ==== ${message}`)
+    ),
+    transports: [
+        // log in console of chrome
+        new transports.Console({
+            level: 'error',
+        }),
 
-    // split file daily and save width limit size
-    new transports.DailyRotateFile({
-      filename: 'release/log/wemake-%DATE%.log',
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: false,
-      maxSize: '20m',
-      maxFiles: '14d'
-    })
-  ],
-  // Handling Uncaught Exceptions with winston
-  exceptionHandlers: [
-    new transports.File({ filename: 'release/log/exceptions.log' })
-  ]
+        // split file daily and save width limit size
+        new transports.DailyRotateFile({
+            filename: `${logFolder}/ofweek-%DATE%.log`,
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: false,
+            maxSize: '2m',
+            maxFiles: '7d'
+        })
+    ],
+    // Handling Uncaught Exceptions with winston
+    exceptionHandlers: [
+        new transports.File({ filename: `${logFolder}/exceptions.log` })
+    ]
 })
 
 // @ts-ignore custom transport
 class ApiTransport extends Transport {
-  constructor(opts: any) {
-    console.log('add transport==============>', opts)
-    super(opts)
-  }
+    constructor(opts: any) {
+        console.log('add transport==============>', opts)
+        super(opts)
+    }
 
-  log(info: any, callback: () => void) {
-    // this will be happend once winston receive log
-    console.log('info=======>>>>>>>', info)
-    callback()
-  }
+    log(info: any, callback: () => void) {
+        // this will be happend once winston receive log
+        console.log('info=======>>>>>>>', info)
+        callback()
+    }
 }
 
 const levelArray: Array<any> = Object.keys(levels)
 const logFun: any = (namespace?: string) => levelArray.reduce((prev, level) => {
-  prev[level] = (...args: any) => wemakeLog[level](`${chalk.cyan(namespace)} ### ${util.format(...args)}`)
-  return prev
+    prev[level] = (desc: string, ...args: any) => ofweekLog[level](`${chalk.cyan(namespace)} ### ${chalk.red(desc)} ${util.format(...args)}`)
+    return prev
 }, {})
 
 export default logFun
