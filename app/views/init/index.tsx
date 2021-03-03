05cff19bc05df3bf79ddbf1dd3e882e2a2a2c07b
lpg-kobe <2638016746@qq.com>
2021-03-04 01:16:57 +0800

    feat:添加驱动检测

--- app/views/init/index.tsx
+++ app/views/init/index.tsx
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next'
 import { Button, Select, Tag, Divider, Steps } from 'antd'
+import { exec, execFile } from 'child_process'
+import path from 'path'
 import logger from "../../utils/log"
 import { rendererInvoke, MAIN_EVENT } from '../../utils/ipc';
 import { judgeRouterUrl } from '../../utils/tool';
-import { DEFAULT_WINDOW_SIZE } from '../../constants';
+import { DEFAULT_WINDOW_SIZE, NODE_ENV, RESOURCES_PATH } from '../../constants';
 
-const log = logger('______Init Page______')
+const wemakeLog = logger('______Init Page______')
 const Init = (props: any) => {
   const [serialports, setSerialports]: any = useState([])
+  const [step, setStep]: any = useState(0)
   const { t } = useTranslation()
-  setTimeout(() => {
-    rendererInvoke(MAIN_EVENT.MAIN_OPEN_PAGE, {
-      ...DEFAULT_WINDOW_SIZE.MAIN,
-      namespace: 'homeWindow',
-      closeNamespace: 'initWindow',
-      url: judgeRouterUrl('/home'),
-    }, () => {
 
+  useEffect(() => {
+    // check driver if exist
+    const isMac = process.platform === 'darwin'
+    const command = isMac ? 'ls /System/Library/Extensions' : 'driverquery'
+    const driverName = isMac ? 'mac-os-x-driver' : 'setup-driver'
+    exec(command, (error, stdout) => {
+      if (error) {
+        wemakeLog.info(`fail by node command exec:`, error)
+        return
+      }
+      const isInstalled = stdout.includes(driverName)
+      const driverPath = NODE_ENV === 'production' ? path.join(RESOURCES_PATH, '/resources/cache') : path.join(__dirname, '../', 'resources/node.pkg')
+      if (isInstalled) {
+        setStep(step + 1)
+      } else {
+        execFile(driverPath, (error, stdout) => {
+          if (error) {
+            wemakeLog.info(`fail by node command execFile:`, error)
+            return
+          }
+          console.log(stdout)
+        })
+      }
     })
-  }, 3000)
+  }, [])
+
+  // rendererInvoke(MAIN_EVENT.MAIN_OPEN_PAGE, {
+  //   ...DEFAULT_WINDOW_SIZE.MAIN,
+  //   namespace: 'homeWindow',
+  //   closeNamespace: 'initWindow',
+  //   url: judgeRouterUrl('/home'),
+  // }, () => {
+
+  // })
 
   return (
     <div className="init-page-container main-container">
@@ -34,8 +62,8 @@ const Init = (props: any) => {
       </section>
       <Divider orientation="left"></Divider>
       <section>
-        <Steps current={1} percent={60}>
-          <Steps.Step disabled status="wait" title={t('detect drive')} description={t('find drive to install')} />
+        <Steps current={step} percent={100 / 3 * (step + 1)}>
+          <Steps.Step disabled title={t('detect drive')} description={t('find drive to install')} />
           <Steps.Step disabled title={t('connect port')} subTitle={t('connecting')} description={t('ready to connect...')} />
           <Steps.Step disabled title={t('finish connect')} description={t('connect finished to start')} />
         </Steps>
